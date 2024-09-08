4c0830e62e51ff36bac2414e19b7a20a50dbb5f2
Noah Cardoso <106560551+NoahCardoso@users.noreply.github.com>
2024-09-07 23:01:20 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
